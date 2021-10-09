b5c47d35270288b48e9f7acdb0ccb39c83a4fd36
AMIrene <f.adammanik@gmail.com>
2021-10-09 15:12:35 +1100

    redid file structure to start from scratch

--- src/App.js
+++ src/App.js
@@ -1,17 +1,6 @@
-import './App.css';
-import Navbar from './components/navbar';
+import React from "react";
+import PortfolioContainer from "./components/PortfolioContainer";
 
-function App() {
- const title = "Irene Adam Manik"
-  return (
-    <div className="App">
-      <h1>{ title}</h1>
-      <Navbar/>
-      <div className="content">
-        
-      </div>
-    </div>
-  );
-}
+const App = () => <PortfolioContainer />;
 
 export default App;
