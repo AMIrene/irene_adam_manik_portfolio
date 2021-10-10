5033f8378127ac7285b1a95b9d01a0e37f0cc457
AMIrene <f.adammanik@gmail.com>
2021-10-10 16:57:58 +1100

    styled nav bar

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -1,61 +1,33 @@
 import React from 'react';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar } from 'react-bootstrap';
+import logo from '../components/img/iamlogo.png';
 
 function NavTabs({ currentPage, handlePageChange }) {
   return (
-    <ul className="nav nav-tabs">
-      <li className="nav-item">
-        <a
-          href="#home"
-          onClick={() => handlePageChange('Home')}
+    <Navbar bg="dark" variant="dark" expand="lg">
 
-          className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
-        >
-          Home
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#about"
-          onClick={() => handlePageChange('About')}
+      <Navbar.Brand href="#home"
+        onClick={() => handlePageChange('Home')}
+        className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}> <img src={logo}/>Irene Adam Manik</Navbar.Brand>
 
-          className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
-        >
-          About
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#work"
-          onClick={() => handlePageChange('Work')}
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
 
-          className={currentPage === 'Work' ? 'nav-link active' : 'nav-link'}
-        >
-          Work
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#contact"
+      <Navbar.Brand href="#about" onClick={() => handlePageChange('About')}
+          className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}>About</Navbar.Brand>
+        
+        <Navbar.Brand href="#work" onClick={() => handlePageChange('Work')}
+          className={currentPage === 'Work' ? 'nav-link active' : 'nav-link'}>Work</Navbar.Brand>
+        
+        <Navbar.Brand href="#contact" onClick={() => handlePageChange('Contact')}
+          className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}>Contact</Navbar.Brand>
+        
+        <Navbar.Brand href="#contact" onClick={() => handlePageChange('CV')}
+          className={currentPage === 'CV' ? 'nav-link active' : 'nav-link'}>Resume</Navbar.Brand>
 
-          onClick={() => handlePageChange('Contact')}
-          className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
-        >
-          Contact
-        </a>
-      </li>
-
-      <li className="nav-item">
-        <a
-          href="#cv"
-
-          onClick={() => handlePageChange('CV')}
-          className={currentPage === 'CV' ? 'nav-link active' : 'nav-link'}
-        >
-          CV
-        </a>
-      </li>
-    </ul>
+      </Navbar.Collapse>
+      </Navbar>
   );
 }
 
