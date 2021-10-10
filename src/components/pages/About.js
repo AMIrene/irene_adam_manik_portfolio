e0fb3bf55140f56d30766d421a01b4bbdee07ca8
AMIrene <f.adammanik@gmail.com>
2021-10-10 23:29:20 +1100

    adding cards for projects

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -9,14 +9,30 @@ import { Col } from 'react-bootstrap';
 
 
 
+
 export default function About() {
   return (
-    <Container className="skills d-flex justify-content-center">
+    <Container >
       <Row>
-      <Col className="codingskills" sm>HTML</Col>
-    <Col className="codingskills" sm>CSS</Col>
-    <Col className="codingskills" sm>JavaScript</Col>
-    </Row>
+        <Col className="codingskills" sm>
+       HTML
+       </Col>
+        <Col className="codingskills" sm>
+        CSS
+    </Col>
+        <Col className="codingskills" sm>
+       Javascript
+        </Col>
+        
+      </Row>
+      
+
+      <Row>
+        <Col className="codingskills" sm>
+          <p> A budding web developer with a strong interest in creating user-friendly web applications. I currently work as a project coordinator for a social research company in the monitoring and evaluation space. I am new to coding and learning HTML, CSS and JavaScript. The thrill of discovery when you figure out a solution to a problem keeps me motivated. However, what I truly love is the design process and the creative aspect. My first foray into web development and UX/UI design was in my previous role, where I coordinated the upgrade of a data visualisation tool for a major census in Australia. My aim now is to be able to apply the skills gained through the Monash Bootcamp course and develop user-driven apps to support data collection and create engaging data visualisations.</p>
+        </Col>
+    
+      </Row>
   </Container>
   );
 }
--- src/components/pages/Work.js
+++ src/components/pages/Work.js
@@ -1,24 +1,110 @@
 import React from 'react';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Card } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
+import { Row } from 'react-bootstrap';
+import { Col } from 'react-bootstrap';
+import '../../styles/section.css';
+
+
+
 export default function Blog() {
   return (
     <div>
-      <h1>My projects</h1>
-      <p>
-        Donec a volutpat quam. Curabitur nec varius justo, sed rutrum ligula.
-        Curabitur pellentesque turpis sit amet eros iaculis, a mollis arcu
-        dictum. Ut vel ante eget massa ornare placerat. Etiam nisl orci, finibus
-        sodales volutpat et, hendrerit ut dolor. Suspendisse porta dictum nunc,
-        sed pretium risus rutrum eget. Nam consequat, ligula in faucibus
-        vestibulum, nisi justo laoreet risus, luctus luctus mi lacus sit amet
-        libero. Class aptent taciti sociosqu ad litora torquent per conubia
-        nostra, per inceptos himenaeos. Mauris pretium condimentum tellus eget
-        lobortis. Interdum et malesuada fames ac ante ipsum primis in faucibus.
-        Donec placerat accumsan mi, ut congue neque placerat eu. Donec nec ipsum
-        in velit pellentesque vehicula sit amet at augue. Maecenas aliquam
-        bibendum congue. Pellentesque semper, lectus non ullamcorper iaculis,
-        est ligula suscipit velit, sed bibendum turpis dui in sapien.
-      </p>
+ <Container >
+      <Row>
+        <Col className="codingskills" sm>
+        <Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="https://cdn.pixabay.com/photo/2018/01/01/01/56/yoga-3053488_960_720.jpg" />
+  <Card.Body>
+    <Card.Title>BeeStrong: Fitness App</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+   
+  </Card.Body>
+</Card>
+       </Col>
+        <Col className="codingskills" sm>
+        <Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="https://cdn.pixabay.com/photo/2017/12/29/19/37/food-3048440_960_720.jpg" />
+  <Card.Body>
+    <Card.Title>ChopNChange: Recipe App</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+   
+  </Card.Body>
+</Card>
+    </Col>
+        <Col className="codingskills" sm>
+        <Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="https://cdn.pixabay.com/photo/2019/01/17/19/11/registration-3938434_960_720.jpg" />
+  <Card.Body>
+    <Card.Title>Password Generator</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+   
+  </Card.Body>
+</Card>
+        </Col>
+        
+      </Row>
+      
+        
+      <Row>
+        <Col className="codingskills" sm>
+        <Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="https://images.unsplash.com/photo-1625813948790-936f256faea8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80" />
+  <Card.Body>
+    <Card.Title>Note Taker</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+   
+  </Card.Body>
+</Card>
+       </Col>
+        <Col className="codingskills" sm>
+        <Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="https://images.unsplash.com/photo-1427751840561-9852520f8ce8?ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8YnVkZ2V0fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" />
+  <Card.Body>
+    <Card.Title>Budget Tracker</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+   
+  </Card.Body>
+</Card>
+    </Col>
+        <Col className="codingskills" sm>
+        <Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="https://images.unsplash.com/photo-1576004275013-8dd7cfe3aaf2?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fHBsYW5uZXIlMjBtb2JpbGV8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" />
+  <Card.Body>
+    <Card.Title>Day Scheduler</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+   
+  </Card.Body>
+</Card>
+        </Col>
+        
+      </Row>
+      
+
+     
+  </Container>
+
+      
     </div>
   );
 }
